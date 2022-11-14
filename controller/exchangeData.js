f6a91aacfaabc1026150343c92490230cf78325e
rajneesharya94 <rajneesharya94@gmail.com>
2022-11-14 08:12:37 +0000

    a

--- controller/exchangeData.js
+++ controller/exchangeData.js
@@ -8,6 +8,10 @@ function roundOffHigherFunc (minutes, date = new Date()){
     const ms = 1000 * 60 * minutes;
       return new Date(Math.ceil(date.getTime() / ms) * ms);
 }
+function roundOffLowerFunc (minutes, date = new Date()){
+    const ms = 1000 * 60 * minutes;
+      return new Date(Math.floor(date.getTime() / ms) * ms);
+}
 
 function getTimeArr (start, end, minutes) {
     let arr = []
@@ -15,7 +19,8 @@ function getTimeArr (start, end, minutes) {
     // while(i=diff){
         
         let roundOffStart = roundOffHigherFunc(minutes, start)
-        // arr.push(roundOffStart)
+        let roundOffPreStart = roundOffLowerFunc(minutes,start)
+        arr.push(roundOffPreStart)
         // i++
     
     
@@ -24,7 +29,7 @@ function getTimeArr (start, end, minutes) {
         arr.push(roundOffStart)
         roundOffStart = new Date(roundOffStart)
         roundOffStart.setMinutes(roundOffStart.getMinutes()+minutes)
-        console.log("???", arr, diff,i, roundOffStart)
+        // console.log("???", arr, diff,i, roundOffStart)
     }
     return arr
 }
@@ -35,10 +40,10 @@ export const exchangeData = async(req, res) => {
 
     startTime = new Date(startTime)
     endTime = new Date(endTime)
-    // console.log(startTime, endTime)
+    console.log(startTime, endTime)
 
     
-    // console.log("controller", collectionName)
+    console.log("controller", collectionName)
 
    db.collection(collectionName).aggregate([
         {
@@ -49,16 +54,52 @@ export const exchangeData = async(req, res) => {
         }
 
     ]).toArray().then(r=>{
-        // console.log(r)
+        console.log("????r??",r)
+        
         let openArr = []
+        let closeArr = []
+        let highArr = []
+        let lowArr = []
+        let volArr = []
         let timeArr = getTimeArr(startTime, endTime,duration )
+        timeArr.forEach(item=>{
             console.log("timearr", timeArr)
-            timeArr.forEach(item=>{
+            let index
+            let flag = true
+                for(let i=0; i<r.length; i++ ){
+                    console.log("condition",item.valueOf()==r[i].time.valueOf() )
+                    if(item.valueOf()==r[i].time.valueOf()){
+                        flag = false
+                        openArr.push(r[i].open)
+                        closeArr.push(r[i].close)
+                         highArr.push(r[i].high)
+                        lowArr.push(r[i].low)
+                        volArr.push(Math.round(r[i].volume * 100)/100)
+                    }
+                    
+                }
+                console.log("flag",flag)
+                if(flag) {
+                        openArr.push(null)
+                        closeArr.push(null)
+                        highArr.push(null)
+                        lowArr.push(null)
+                        volArr.push(null)
+
+                }
+                // else if(r[index]){
+                //     openArr.push(r[index].open)
+                //     closeArr.push(r[index].close)
+                //      closeArr.push(r[index].close)
+                //     lowArr.push(r[index].low)
+                //     volArr.push(r[index].volume)
+                // }
                 
             })
             
             
-            return res.send({'result':timeArr})
+            
+            return res.send({'result':openArr,closeArr,highArr,lowArr,volArr})
     })
     
 
